29b8c6774ea70d1677810da25328292b35996125
Sandra VOISIN <sandra.voisin92@gmail.com>
2023-10-30 13:28:58 +0100

    Travailler avec les différents types de nombres

---
+++ MelangerNombres.java
@@ -0,0 +1,17 @@
+public class MelangerNombres {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+
+        // int c = a / b; --> "Result : 2"
+        // float c = a / b --> "Result : 2.0"
+
+        // Converti la variable a en float
+        float c = (float) a / b;
+
+        // Converti la variable b en float
+        // float c = a / (float) b;
+
+        System.out.println(c);
+    }
+}
