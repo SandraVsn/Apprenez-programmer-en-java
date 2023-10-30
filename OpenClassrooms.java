9d23b02c73cfbcc93760e6c7d9ccce487545dd9b
Sandra VOISIN <sandra.voisin92@gmail.com>
2023-10-30 11:04:52 +0100

    use variables

---
+++ OpenClassrooms.java
@@ -0,0 +1,13 @@
+public class OpenClassrooms {
+    public static void main(String[] args) {
+        int agePersonne = 19;
+
+        agePersonne = 33;
+
+        agePersonne = agePersonne + 28;
+
+        agePersonne += 1;
+
+        System.out.println("Age = " + agePersonne + " ans");
+    }
+}
