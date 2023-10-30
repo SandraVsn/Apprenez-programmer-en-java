bc5a768d7368d1b2ad91509185fc3e0f04f725bb
Sandra VOISIN <sandra.voisin92@gmail.com>
2023-10-30 13:28:48 +0100

    Manipuler les variables

---
+++ ManipulationVariables.java
@@ -0,0 +1,20 @@
+public class ManipulationVariables {
+
+    public static void main(String[] args) {
+        int epargne = 500;
+        int revenus = 2000;
+        // Ajoutez 100 à votre épargne (Yeah!)
+        epargne = epargne + 100;
+
+        // Enlevez 50 à votre indemnité (Snif)
+        revenus = revenus - 50;
+        // Faites une mise à jour sur votre délai d'épargne
+        int nombreDeJoursEpargne = (5000 - revenus) / epargne;
+
+        // Mettez à jour à nouveau votre indemnité (encore)
+        revenus = revenus + (30 - 10) * 7;
+
+        System.out.println(nombreDeJoursEpargne);
+
+    }
+}
